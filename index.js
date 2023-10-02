9843352ae5f0d8c50097c1c68450f83b6a37eeac
alex-yellow <hitys2000@gmail.com>
2023-10-02 13:48:59 +0300

    output all tasks

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
-app.listen(PORT, function(){
-    console.log('Server is running on port', PORT);
-});
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+const mongoclient = new mongodb.MongoClient('mongodb://localhost:27017/');
+
+async function Start(){
+    try {
+        await mongoclient.connect();
+        console.log('connection success');
+        const db = mongoclient.db('todos');
+        const coll = db.collection('tasks');
+        try {
+            app.get('/', async function(req, res){
+                const tasks = await coll.find().toArray();
+                res.render('index', {title: 'Home', tasks});
+            });
+        } catch (error) {
+            console.log('error');
+        }
+        app.listen(PORT, function(){
+            console.log('Server is running on port', PORT);
+        });
+    } catch (error) {
+        console.log('connection error', error);
+    }
+}
+
+Start();
